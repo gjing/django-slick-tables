0e2779250421a43eb36b6f82c9f86e68ed72ceab
gjing <georgejing93@gmail.com>
2025-10-01 22:43:56 -0700

    mypy fixes

--- slick_tables/headers.py
+++ slick_tables/headers.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 class Header():
@@ -6,7 +6,7 @@ class Header():
     template_name = "slick_tables/headers/default.html"
 
     def render(self) -> str:
-        return render(self.template_name, {"header": self})
+        return render_to_string(self.template_name, {"header": self})
 
     class Meta:
         abstract = True
