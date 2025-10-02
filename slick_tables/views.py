33ed95cfa8cc82dcafbd5ac23ea90437c6adac1e
gjing <georgejing93@gmail.com>
2025-10-01 22:08:28 -0700

    setup pre commit

--- slick_tables/views.py
+++ slick_tables/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import ListView
 
 # Create your views here.
+class SlickTableListView(ListView):
+    class Meta:
+        abstract = True
+        fields: list[str] = []
